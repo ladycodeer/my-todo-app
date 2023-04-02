1ae5caf0003c8f72ae9da4196a4cc76f6bba2c03
Rabia Kahya <rabiakahya22@gmail.com>
2023-04-03 02:46:08 +0300

    Sould i sleep but it is almost sahoor ı am sleepi mood

--- src/Todo.jsx
+++ src/Todo.jsx
@@ -1,15 +1,25 @@
 import React from 'react'
-const style = {
+import { FaRegTrashAlt } from 'react-icons/fa'
 
+const style = {
+    li: `flex justify-between bg-slate-200 p-4 my-2 capitalize`,
+    liComplete: `flex justify-between bg-slate-400 p-4 capitalize`,
+    row: `flex`,
+    text: `ml-2 cursor-pointer `,
+    textComplete: ` ml-2 cursor-pointer line-through`,
+    button: `cursor-pointer flex items-center`,
 }
 
-function Todo() {
+function Todo({ todo }) {
     return (
         <li className={style.li}>
             <div className='style.row'>
                 <input type="checkbox" />
                 <p className='style.text' >{todo}</p>
             </div>
+            <button className={style.button}>{<FaRegTrashAlt />}</button>
+            <button className='style.textComplete'></button>
+            <h1 ></h1>
         </li>
     )
 }
