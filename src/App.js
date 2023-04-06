2d8159e4bd0eaac9dc9ea9e46afd0091eea5a0a6
Rabia Kahya <rabiakahya22@gmail.com>
2023-04-06 23:14:10 +0300

    reading data from the firebase

--- src/App.js
+++ src/App.js
@@ -1,20 +1,39 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import { AiOutlinePlus } from 'react-icons/ai'
 import Todo from './Todo'
+import { db, } from './firebase'
+import { query, collection, onSnapshot, } from 'firebase/firestore'
 
 const style = {
   bg: `h-screen w-screen p-4 bg-gradient-to-r from-[#2F80ED] to-[#1CB5E0]`,
-  container: ` bg-slate-100 max-w-[500px] w-full m-auto rounded-md shadow-xl p-4`,
+  container: `bg-slate-100 max-w-[500px] w-full m-auto rounded-md shadow-xl p-4`,
   heading: `text-3xl font-bold text-center text-gray-800 p-2`,
   form: `flex justify-between`,
   input: `border p-2 w-full text-xl`,
   button: `border p-4 ml-2 bg-purple-500 text-slate-100`,
-  count: `text-center p-2`
-
-}
+  count: `text-center p-2`,
+};
 
 function App() {
-  const [todos, setTodos] = useState(['Learn React', 'Do the dishes'])
+  const [todos, setTodos] = useState([]);
+  // create todo
+  //read todo from firabase
+  useEffect(() => {
+    const q = query(collection(db, 'todos'))
+    const unsubscribe = onSnapshot(q, (QuerySnapshot) => {
+      let todosArr = []
+      QuerySnapshot.forEach((doc) => {
+        todosArr.push({ ...doc.data(), id: doc.id })
+      });
+      setTodos(todosArr)
+    })
+    return () => unsubscribe()
+
+  }, [])
+
+
+  //update todo in firebase
+  //delete todo
   return (
     <div className={style.bg}>
       <div className={style.container}>
